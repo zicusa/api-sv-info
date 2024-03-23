e636b9bc85e3803cb1a1fb28e91162250f9e33fd
zicusa <zicusa@yahoo.co.uk>
2024-03-23 09:19:03 +0700

    Update index.js

--- index.js
+++ index.js
@@ -1,7 +1,13 @@
 //index.js
 
 function helloAPI() {
-    return "hello NPM API"
+    let id = "";
+    id = getID("AB16XIANXNMA");
+    return id;
   }
-  
+function getID(idstring) {
+    let id = "";
+    id = idstring.substring(1, 6);
+    return id
+  }
 module.exports = helloAPI
